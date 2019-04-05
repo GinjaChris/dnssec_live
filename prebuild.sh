3d6e5cebebdea1033d9950e45691ac9005eb5e91
Chris Spillane <chris.spillane@gmail.com>
2019-04-05 10:26:50 +0100

    Create prebuild.sh

---
+++ prebuild.sh
@@ -0,0 +1,33 @@
+cd ~
+mkdir -p ~/archlive
+cp -r /usr/share/archiso/configs/profile/ archlive
+cd ~/archlive/releng/
+rm airootfs/root/install.txt
+echo "LANG=en_GB.UTF-8" >  airootfs/etc/locale.conf 
+echo "DNSSEC-live" > airootfs/etc/hostname
+#"packages.x86_64" is the packages that will be installed on the liveCD (in addition to everything in the "base" category)
+curl https://raw.githubusercontent.com/GinjaChris/dnssec_live/master/packages.x86_64 -o ./packages.x86_64
+#this copies over the script that will run when the livecd boots
+curl https://raw.githubusercontent.com/GinjaChris/dnssec_live/master/customize_airootfs.sh -o airootfs/root/customize_airootfs.sh
+#set uk keymap
+echo "KEYMAP=uk" > airootfs/etc/vconsole.conf
+mkdir -p airootfs/etc/unbound
+#add unbound config
+curl https://raw.githubusercontent.com/GinjaChris/dnssec_live/master/unbound.conf -o airootfs/etc/unbound/unbound.conf 
+#add pointers to root servers
+curl https://www.internic.net/domain/named.cache -o airootfs/etc/unbound/root.hints
+#add trust for root zone
+curl https://raw.githubusercontent.com/GinjaChris/dnssec_live/master/trusted-key.key -o airootfs/etc/unbound/trusted-key.key
+#set bootup files
+curl https://raw.githubusercontent.com/GinjaChris/dnssec_live/master/ntt.png -o syslinux/splash.png
+curl https://raw.githubusercontent.com/GinjaChris/dnssec_live/master/mkinitcpio.conf -o ./mkinitcpio.conf
+curl https://raw.githubusercontent.com/GinjaChris/dnssec_live/master/archiso.cfg -o syslinux/archiso.cfg
+curl https://raw.githubusercontent.com/GinjaChris/dnssec_live/master/archiso_head.cfg -o syslinux/archiso_head.cfg
+curl https://raw.githubusercontent.com/GinjaChris/dnssec_live/master/archiso_sys.cfg -o syslinux/archiso_sys.cfg
+curl https://raw.githubusercontent.com/GinjaChris/dnssec_live/master/syslinux.cfg -o syslinux/syslinux.cfg
+rm syslinux/archiso_pxe.cfg
+#this has an entry to use old device naming (like "eth0")
+echo "net.ifnames=0" > airootfs/etc/sysctl.d/99-sysctl.conf
+sync && sleep 1
+#build the ISO!  This will take a while.....If anything goes wrong, or you make changes to any of the files and need to rebuild the ISO, remove the work folder with "rm -v work/build.make_*" and run build.sh script again 
+./build.sh  -v 
